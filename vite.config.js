88755278e02f0ed0bd42fb422a8ed0b8560aa7c2
신상우 <bano112@naver.com>
2023-08-16 21:08:04 +0900

    test16

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import vue from '@vitejs/plugin-vue';
+import sass from 'sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vue(), sass()],
   build: {
     rollupOptions: {
       external: ['react-icons/fa6'],
